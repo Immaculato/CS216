3179606f95dd7c8333a57da95ba66102ec0e2f83
Immaculato <tristan.a.basil@gmail.com>
2016-02-13 17:13:33 -0500

    Program 1 compiles, at least

--- Programs/Program1/IntSequence.cpp
+++ Programs/Program1/IntSequence.cpp
@@ -1,45 +1,40 @@
-#include "sortings.h"
-#include "cstdlib"
-#include "iostream"
+#include "IntSequence.h"
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 IntSequence::IntSequence()
 {
-	intArray = new int[INITIAL_CAPACITY];
+	seq = new int[INITIAL_CAPACITY];
 	capacity = INITIAL_CAPACITY;
-	size = 0;
+	count = 0;
 }
 
-IntSequence::IntSequence(in_capacity)
+IntSequence::IntSequence(int in_capacity)
 {
-	intArray = new int[in_capacity];
+	seq = new int[in_capacity];
 	capacity = in_capacity;
-	size = 0;
-}
-
-int IntSequence::IntSequence()
-{
-	return size;
+	count = 0;
 }
 
 void IntSequence::insert(int item)
 {
-	int* tempArray=new int[size+1];
-	for (int i=0; i < size; i++)
+	int* tempArray=new int[count+1];
+	for (int i=0; i < count; i++)
 	{
-		tempArray[i] = intArray[i];
+		tempArray[i] = seq[i];
 	}
-	delete intArray;
-	size+=1;
-	tempArray[size-1] = item;
-	intArray = tempArray;
+	delete seq;
+	count+=1;
+	tempArray[count-1] = item;
+	seq = tempArray;
 }
 
 void IntSequence::insertion_sort()
 {
 /*
-func insertionsort( var A as an array of size n)
+func insertionsort( var A as an array of count n)
 	for i= 1 to n - 1
 		key = A[i]
 		position = i
@@ -54,30 +49,30 @@ end func
 	int key;
 	int position;
 	//for the length of the list,
-	for (int i=1; i<size; i++)
+	for (int i=1; i<count; i++)
 	{
 		//the key is the current position's integer value, and
-		key = intArray[i];
+		key = seq[i];
 		//the current position is the current index.
 		position = i;
 		//while the position is not and beginning, and the value behind is
 		//greater than the value we're looking at,
-		while (position > 0 && intArray[position-1] > key)
+		while (position > 0 && seq[position-1] > key)
 		{
 			//move the greater value one over.
-			intArray[position] = intArray[position-1];
+			seq[position] = seq[position-1];
 			//move back the position to check again.
 			position = position - 1;
 		}
 		//if the correct position is found, put it in and continue.
-		intArray[position] = key;
+		seq[position] = key;
 	}
 }
 
 void IntSequence::bubble_sort()
 {
 /*
-func BubbleSort( var a as an array of size n )
+func BubbleSort( var a as an array of count n )
 	for i from 1 to n-1
 		for j from 0 to n-2
 			if (a[j] > a[j+1])
@@ -86,32 +81,32 @@ func BubbleSort( var a as an array of size n )
 	end for
 end func
 */
-	for (int i=0; i < size; i++)
+	for (int i=0; i < count; i++)
 	{
-		for (int j=0; j<size-1; j++)
+		for (int j=0; j<count-1; j++)
 		{
-			if (intArray[j] > intArray[j+1])
+			if (seq[j] > seq[j+1])
 			{
 				int tempvalue;
-				tempvalue = intArray[j];
-				intArray[j] = intArray[j+1];
-				intArray[j+1] = tempvalue;
+				tempvalue = seq[j];
+				seq[j] = seq[j+1];
+				seq[j+1] = tempvalue;
 			}
 		}
 	}
 }
 
-void sortings::print()
+void IntSequence::print()
 {
-	for (int i=0; i<size; i++)
+	for (int i=0; i<count; i++)
 	{
-		cout << intArray[i] << endl;
+		cout << seq[i] << endl;
 	}	
 }
 
-sortings::~sortings()
+IntSequence::~IntSequence()
 {
-	delete intArray;
+	delete seq;
 }
 
 
---
+++ Programs/Program1/program1.cpp
@@ -0,0 +1,14 @@
+#include "IntSequence.cpp"
+#include <cstdlib>
+
+using namespace std;
+
+int main();
+
+int main()
+{
+	IntSequence listofnumbers;
+	listofnumbers.insert(100);
+	listofnumbers.insert(200);
+	cout << "I wonder if this will compile.";
+}
