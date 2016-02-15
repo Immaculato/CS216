6c29ef040b28f95a93945da210390214d4f6ea58
Immaculato <tristan.a.basil@gmail.com>
2016-02-14 22:34:30 -0500

    program 1 final version!

--- Programs/Program1/IntSequence.cpp
+++ Programs/Program1/IntSequence.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 IntSequence::IntSequence()
 {
+	//make a new, empty int array with INITIAL_CAPACITY
 	seq = new int[INITIAL_CAPACITY];
 	capacity = INITIAL_CAPACITY;
 	count = 0;
@@ -18,6 +19,7 @@ IntSequence::IntSequence()
 
 IntSequence::IntSequence(int in_capacity)
 {
+	//make a new, mepty int array with size in_capacity
 	seq = new int[in_capacity];
 	capacity = in_capacity;
 	count = 0;
@@ -25,24 +27,38 @@ IntSequence::IntSequence(int in_capacity)
 
 void IntSequence::insert(int item)
 {
+	//make a new temporary array with 1 greater size than the current array
 	int* tempArray=new int[count+1];
+	//for each element in the current array,
 	for (int i=0; i < count; i++)
 	{
+		//swap over the value to the temporary array.
 		tempArray[i] = seq[i];
 	}
+	//delete the current array,
 	delete seq;
+	//increment the size counter for the new element,
 	count+=1;
+	//actually insert the new item into the array, and finally,
 	tempArray[count-1] = item;
+	//make seq point to the new array.
 	seq = tempArray;
 }
 
 void IntSequence::print()
 {
+	//for each item in seq, print it.
 	for (int i=0; i<count; i++)
 	{
 		cout << seq[i] << " ";
 	}
+	//if there are no items to print, say so.
+	if (count == 0)
+	{
+		cout << "Empty array! Read in some values.";
+	}
 	cout << endl;	
+
 }
 
 void IntSequence::selection_sort()
@@ -50,42 +66,59 @@ void IntSequence::selection_sort()
 	cout << "===Selection Sort===================================\n";
 	int minimum;
 	int jmin;
+	//for each item in the array,
 	for (int i=0; i<count; i++)
 	{
+		//set the minimum to the first item in the subarray.
 		minimum = seq[i];
+		//for each item in the subarray,
 		for (int j=i; j<count; j++)
 		{
+			//if a value is less than the minimum,
 			if (seq[j] < minimum)
 			{
+				//make that value the new minimum,
 				minimum = seq[j];
+				//and make note of what index it happened at.
 				jmin = j;
 			}
 		
 		}
+		//if the minimum wasn't the first item in the subarray,
 		if (minimum != seq[i])
 		{
+		//say that you swapped it, and what with.
 		cout << "Min " << minimum << ", swap with " << seq[i] << ":\t";
 		}
+		//if the first item in the subarray wasn't swapped,
 		else
 		{
+		//say so.
 		cout << "Min " << minimum << ", no swap:\t";
 		}
+		//if a smaller value was found,
 		if (jmin != -1)
 		{
+			//swap the values of the minimum and first element.
 			seq[jmin] = seq[i];
 			seq[i]=minimum;
 		}
+		//print out the results, and reset jmin to keep sorting.
 		print();
 		jmin = -1;
 	
 		
 	}
+	//finally, output the sorted array.
 	cout << "Sequence:\t\t";
 	print();
 }
 
 void IntSequence::sort()
 {
+	//NOTE: this is functionally identical to the selection sort above,
+	//but has no standard output. this method is used when the array must
+	//be quietly sorted before the binary serach can be performed.
 	int minimum;
 	int jmin;
 	for (int i=0; i<count; i++)
@@ -166,17 +199,23 @@ end func
 */
 	cout << "===Bubble Sort===================================\n";
 	int counter = 0;
+	//for each element in the array, and
 	for (int i=0; i < count; i++)
 	{
+		//for each element before the end,
 		for (int j=0; j<count-1; j++)
 		{
+			//if the current element is greater than the next one,
 			if (seq[j] > seq[j+1])
 			{
+				//swap the values,
 				int tempvalue;
 				tempvalue = seq[j];
 				seq[j] = seq[j+1];
 				seq[j+1] = tempvalue;
+				//Increment the counter,
 				counter+=1;
+				//output the iteration number.
 				cout << "Iteration " << counter << ":\t";
 				print();
 			}
@@ -188,13 +227,14 @@ end func
 
 void IntSequence::shuffle()
 {
+	//seed with the current time.
 	srand(time(0));
 	for (int i = 0; i < count; i++)
     	{
-        	// Pick a random index from 0 to i
+        	//Pick a random index from 0 to i
         	int j = rand() % (i+1);
  
-        	// Swap arr[i] with the element at random index
+        	//Swap seq[i] with the element at random index
         	swap(seq[i], seq[j]);
     	}
 }
@@ -204,22 +244,29 @@ void IntSequence::sequential_search(int key)
 	bool found = false;
 	int location;
 	int i=0;
+	//while the key hasn't been found and all values have not been exhausted,
 	while (found == false && i < count)
 	{
+		//if the key is found,
 		if (key == seq[i])
 		{
+			//mark the location, and report that it's found.
 			location = i;
 			found = true;
 		}
 		i++;
 	}
+	//if the value was found,
 	if (found)
 	{
+		//say so, and where.
 		cout << "Key found at index " << location << endl;
 		
 	}
+	//if it wasn't found,
 	else
 	{
+		//report so.
 		cout << "Key not found." << endl;
 	}
 	cout << "Search with " << i << " comparison(s)." << endl;
@@ -247,17 +294,17 @@ void IntSequence::binary_search(int key)
 		// calculate midpoint to cut set in half
 		middle = ((higher + lower)/2);
 
-		if (seq[middle] > key)				//if the word in the middle is after the word,
+		if (seq[middle] > key)	//if the value in the middle is after the key,
 		{
-			//return binary_search(key, lower, middle-1);   //make 1 less than the middle the new max, and search again.
+			//make 1 less than the middle the new max, and search again.
 			higher = middle-1;
 		}
-		else if (seq[middle] < key)			//if the word in the middle is before the word,
+		else if (seq[middle] < key)  //if the value in the middle is before the key,
 		{
-			//return binary_search(key, middle+1, higher);	//make 1 more than the middle the new lower index.
+			//make 1 more than the middle the new lower index.
 			lower = middle+1;
 		}
-		else     //if the word in the middle equals the word,
+		else     //if the value in the middle equals the word,
 		{
 			location = middle;
 			isitdone = true;  //you've found it!!
@@ -265,15 +312,20 @@ void IntSequence::binary_search(int key)
 	}
 	counter+=1;
 	}
+	//if the value was found,
 	if (location != -1)
 	{
+		//say so.
 		cout << "Key found at index " << location << endl;
 	
 	}
+	//if it wasn't
 	else
 	{
+		//say so.
 		cout << "Key not found." << endl;
 	}
+	//report how long it took, too.
 	cout << "Search with " << counter << " comparison(s)." << endl;
 }
 
--- Programs/Program1/program1.cpp
+++ Programs/Program1/program1.cpp
@@ -22,18 +22,21 @@ int main()
 	   cout << "=================================================" << endl;
 	   cout << "1. Read\n2. Print\n3. Sort\n4. Shuffle\n5. Search\n6. Quit\nOption: ";
 	   cin >> choice;
+		//if the user wants to read in values,
 		if (choice == '1')
 		{
 	 	 breakflag = false;
+		 //while the user is providing values,
 		 while (!breakflag)
 		 {
-			//keep reading lines from the file and reversing them.
+			//keep reading lines from the file and inserting them.
 			cout << "Enter the next element (Enter 'q' to stop): ";
 			cin >> line;
 			if (line != "q")
 			{
 				listofnumbers.insert(stoi(line));
 			}
+			//if they input q, stop taking in new values.
 			else
 			{
 				breakflag=true;
@@ -41,80 +44,108 @@ int main()
 			
 		 }
 		}
+		//if the user wants to print values,
 		else if (choice == '2')
 		{
+		 //print them.
 		 cout << "Sequence:\t";
 		 listofnumbers.print();
 		}
+		//if the user wants to sort,
 		else if (choice == '3')
 		{
 		 cout << "1. Insertion sort\n2. Selection sort\n3. Bubble sort\n4. Quit\nOption: ";
 		 cin >> line;
+		 //if the user wants to do an insertion sort,
 		 if (line == "1")
 		 {
+			//do it.
 			listofnumbers.insertion_sort();
 		 }
+		 //if the user wants to do a selection sort
 		 else if (line == "2")
 		 {
 			listofnumbers.selection_sort();
 		 }
+		 //if the user wants to do a buble sort
 		 else if (line == "3")
 		 {
 			listofnumbers.bubble_sort();
 		 }
+		 //if the user has no clue what they're doing,
 		 else if (line != "4")
 		 {
+			//tell them.
 			cout << "Invalid option." << endl;
 		 }
+		 //if they want to quit,
 		 else
 		 {
+			//continue back to the main menu.
 			continue;
 		 }
 		}
+		//if the user wants to shuffle the values,
 		else if (choice == '4')
 		{
+		 //shuffle them, and
 		 listofnumbers.shuffle();
+		 //display the new sequence.
 		 cout << "Shuffled sequence:\t";
 		 listofnumbers.print();
 		}
+		//if the user wants to search,
 		else if (choice == '5')
 		{
 		 cout << "1. Sequential search\n2. Binary search \n3. Quit\nOption: ";
 		 cin >> line;
+		 //if the user wants to do a sequential search,
 		 if (line == "1")
 		 {
+			//get a value to look for, and do it.
 			cout << "Enter the key to find: ";
 			int key;
 			cin >> key;
 		 	listofnumbers.sequential_search(key);
 		 }
+		 //if the user wants to do a binary search,
 		 else if (line == "2")
 		 {
+			//get a value to look for,
 			cout << "Enter the key to find: ";
                         int key;
                         cin >> key;
 			cout << "Unsorted sequence: ";
 			listofnumbers.print();
+			//quietly sort the array, and
 			cout << "Sorted sequence: ";
 			listofnumbers.sort();
 			listofnumbers.print();
+			//do the binary search.
 			listofnumbers.binary_search(key);
 		 }
+		 //if the user has no clue what they're doing,
 		 else if (line != "3")
 		 {
+			//say so.
 			cout << "Invalid option." << endl;
 		 }
+		 //otherwise,
 		 else
 		 {
+			//go back to the main menu.
 			continue;
 		 }
 		}
+		//if the user has no idea what they're doing,
 		else if (choice != '6')
 		{
+			//tell them.
 			cout << "Invalid input." << endl;
 		}
 
-      } while(choice != '6');
+      } while(choice != '6'); //when the user has quit,
+	//thank them for using the program and end it.
 	cout << "Thank you for using this program." << endl;
 	cout << "=================================================" << endl;		
 }
